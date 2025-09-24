f1e5e97f8a3a08b57447300ac8f42949b2887c93
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-09-24 12:19:22 -0700

    12:19 PM 9/24/2025

--- Lab-14.cpp
+++ Lab-14.cpp
@@ -6,11 +6,39 @@ class Color{
     int R;
     int G;
     int B;
+    public:
+    void setRGB(int cR,int cG,int cB){
+        R=cR;
+        G=cG;
+        B=cB;
+    }
+    int getR(){
+        return R;
+    }
+    int getG(){
+        return G;
+    }
+    int getB(){
+        return B;
+    }
 };
+void print(Color C){
+    cout<<endl;
+    cout<<""<<endl;
+    cout<<C.getR()<<endl;
+    cout<<C.getG()<<endl;
+    cout<<C.getB()<<endl;
+}
 
 int main(){
-
-
+    Color Aqua;
+    Color Yellow;
+    Color Purple;
+    Color Black;
+    Color White;
+    Color Red;
+    red.setRGB(3,4,5);
+    print(red);
 
 
     return 1;
